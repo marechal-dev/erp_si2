601e95ffae446c878aa10ee365453d3233d0f430
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-06-23 21:24:46 -0300

    feat: Create handle edit product script

---
+++ src/scripts/storageView/handleEditProduct.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { LocalStorageManager } from "../../classes/utils/LocalStorageManager.js";
+
+const updateItemButtonsList = document.querySelectorAll('.editItemButton');
+
+updateItemButtonsList.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const productId = event.target.id;
+
+    const newItemData = {};
+
+    const itemToEdit = LocalStorageManager.edit(
+      'products', 
+      productId,
+      newItemData, 
+    );
+
+    alert(`Item ${itemToEdit.title} alterado com sucesso!`);
+
+    location.reload();
+  });
+});
